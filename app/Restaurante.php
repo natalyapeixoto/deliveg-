de2a8fff5366e91ed6f7bf6eda6eda441ee78a69
ricmig <ricmig@hotmail.com>
2019-05-25 11:44:15 -0300

    banco de dados criado

---
+++ app/Restaurante.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurante extends Model
+{
+    protected $table = 'restaurante';
+    protected $primaryKey = 'id_restaurante';
+}
