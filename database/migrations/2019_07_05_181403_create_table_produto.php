63ce734d33dd517bc68e22a053ad9bee8c30287e
ricmig <ricmig@hotmail.com>
2019-07-05 15:30:07 -0300

    criado a migrate das tabelas que estavam faltando

---
+++ database/migrations/2019_07_05_181403_create_table_produto.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProduto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->bigIncrements('id_produto');
+            $table->integer('id_categoria');
+            $table->string('nome');
+            $table->longText('descricao');
+            $table->string('foto');
+            $table->float('preco');
+            $table->integer('estoque');
+            $table->foreign('id_categoria')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
