6ecfc0deb92957033bd7ffea4aad033562f3bf5d
natalyapeixoto <natalya_peixoto@hotmail.com>
2019-07-08 19:20:44 -0300

    css fix

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,12 +7,11 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Deliveg') }}</title>
   <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/loja.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/carrinho.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/pagamento.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/perfil.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/login.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('assets/css/cadastro.css') }}">
+  <link rel="stylesheet" href="../../../public/css/loja.css">
+  <link rel="stylesheet" href="../../../public/css/pagamento.css">
+  <link rel="stylesheet" href="../../../public/css/login.css">
+  <link rel="stylesheet" href="../../../public/css/cadastro.css">
+
 </head>
 <body>
 
