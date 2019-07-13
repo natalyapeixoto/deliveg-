f698310ab0bff435609b4f3ac1be53c46ffc39f6
natalyapeixoto <natalya_peixoto@hotmail.com>
2019-07-13 18:56:55 -0300

    criei pagina sucesso

---
+++ resources/views/sucesso.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.base')
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Carrinho</title>
+
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="../css/sucesso.css">
+    <script src="../js/app.js"></script>
+</head>
+
+<body>
+
+<div class="sucesso">
+        <img class="gifSucesso" src="./img/sucesso.gif">
+        <h1>Sucesso!</h1> 
+        <p>Seu pedido está sendo encaminhado</p>
+</div>
+
+</body>
+</html>
+            
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ Route::get('/pagamento', function(){
     return view('pagamento');
 });
 
+Route::get('/sucesso', function(){
+    return view('sucesso');
+});
+
 Route::post('/pagamentos', 'PedidoController@pagar');
 
 Route::get('/pagapedido', 'PedidoController@pagamento');
