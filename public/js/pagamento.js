bcb1cc27d97b785976998e26e2b1bfb12ca70116
natalyapeixoto <natalya_peixoto@hotmail.com>
2019-07-13 21:37:02 -0300

    corrige js pagamento

--- public/js/pagamento.js
+++ public/js/pagamento.js
@@ -42,27 +42,43 @@ const cardNumber = document.getElementById("cardNumber")
 cardNumber.addEventListener("blur", function(){
   if (cardNumber.value.length < 16) {
     document.getElementById("erroCardNumber").innerHTML="Digite um número de cartão válido"
+    document.getElementById("erroCardNumber").style.color = 'red'
+    document.getElementById("erroCardNumber").style.display ='block';
+    document.getElementById("erroCardNumber").style.fontSize = '12px';
   }
+  document.getElementById("erroCardNumber").innerHTML=""
 })
 
 const cardMes = document.getElementById("cardMes") 
 cardMes.addEventListener("blur", function(){
-  if (cardMes.value.length < 2 && cardNumber.value <= 12) {
+  if (cardMes.value.length < 2 && cardMes.value <= 12) {
     document.getElementById("erroCardMes").innerHTML="Mês inválido"
+    document.getElementById("erroCardMes").style.color = 'red'
+    document.getElementById("erroCardMes").style.display ='block';
+    document.getElementById("erroCardMes").style.fontSize = '12px';
   }
+  document.getElementById("erroCardMes").innerHTML=""
 })
 
 const cardAno = document.getElementById("cardAno") 
 cardAno.addEventListener("blur", function(){
   if (cardAno.value.length < 2 && cardAno.value >= 19) {
     document.getElementById("erroCardAno").innerHTML="Ano inválido"
+    document.getElementById("erroCardAno").style.color = 'red'
+    document.getElementById("erroCardAno").style.display ='block';
+    document.getElementById("erroCardAno").style.fontSize = '12px';
   }
+  document.getElementById("erroCardAno").innerHTML=""
 })
 
 const cardCvv = document.getElementById("cardCvv") 
 cardCvv.addEventListener("blur", function(){
   if (cardCvv.value.length < 3) {
     document.getElementById("erroCardCvv").innerHTML="Cvv inválido"
+    document.getElementById("erroCardCvv").style.color = 'red'
+    document.getElementById("erroCardCvv").style.display ='block';
+    document.getElementById("erroCardCvv").style.fontSize = '12px';
   }
+  document.getElementById("erroCardCvv").innerHTML=""
 })
 
